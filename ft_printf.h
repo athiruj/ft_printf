7f261c851f7c29c130424baa0fa1e81f253fc9ea
Athiruj.K <athirujkaewseesuk@gmail.com>
2024-08-30 15:44:50 +0700

    add ft_printf

---
+++ ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: athi <athi@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/29 21:56:03 by athi              #+#    #+#             */
+/*   Updated: 2024/08/29 22:35:50 by athi             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include "libft/libft.h"
+
+// int		ft_printf(const	char *format, ...);
+int		ft_printf(int c, ...);
+
+#endif
